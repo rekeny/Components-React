062a1efe25c4b23cc5303c00d5ec23f460eef277
Fernando Requeni Nacher <fernando.requeni@outlook.com>
2021-02-17 23:37:04 +0000

    created some props

--- src/CommentDetail.js
+++ src/CommentDetail.js
@@ -1,19 +1,20 @@
 import React from 'react';
 
-const CommentDetail = () => {
+const CommentDetail = props => {
+    console.log(props.avatar);
     return (
         <div className='comment'>
             <a href='/' className='avatar'>
-                <img alt="avatar" src='https://source.unsplash.com/random' />
+                <img alt="avatar" src={props.avatar} />
             </a>
             <div className='content'>
                 <a href='/' className='author'>
-                    Sam
+                    {props.author}
                 </a>
                 <div className='metadata'>
-                    <span className='date'>Today at 6.00OPM</span>
+                    <span className='date'>{props.timeAgo}</span>
                 </div>
-                <div className='text'>Nice blog post!!</div>
+                <div className='text'>{props.content}</div>
             </div>
         </div>
     );
--- src/index.js
+++ src/index.js
@@ -5,7 +5,24 @@ import CommentDetail from './CommentDetail';
 const App = () => {
     return (
         <div className="ui container comments">
-            <CommentDetail />            
+            <CommentDetail 
+                author='Fernando' 
+                avatar='https://source.unsplash.com/random' 
+                timeAgo='Today at 3.00PM' 
+                content='Nice blog post!'
+            />            
+            <CommentDetail 
+                author='Alex' 
+                avatar='https://source.unsplash.com/random' 
+                timeAgo='Today at 2.00AM' 
+                content='I like the subject'
+            />            
+            <CommentDetail 
+                author='Jane' 
+                avatar='https://source.unsplash.com/random' 
+                timeAgo='Yesterday at 5.00PM' 
+                content='I like the writing'
+            />            
         </div>
     );
 };
