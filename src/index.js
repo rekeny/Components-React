22754d72ec28a3d593491ff37413cf0da5717d92
Fernando Requeni Nacher <fernando.requeni@outlook.com>
2021-02-17 23:51:49 +0000

    add props.children

--- src/index.js
+++ src/index.js
@@ -6,25 +6,39 @@ import ApprovalCard from './ApprovalCard';
 const App = () => {
     return (
         <div className="ui container comments">
-            <ApprovalCard />
-            <CommentDetail 
-                author='Fernando' 
-                avatar='https://source.unsplash.com/random' 
-                timeAgo='Today at 3.00PM' 
-                content='Nice blog post!'
-            />            
-            <CommentDetail 
-                author='Alex' 
-                avatar='https://source.unsplash.com/random' 
-                timeAgo='Today at 2.00AM' 
-                content='I like the subject'
-            />            
-            <CommentDetail 
-                author='Jane' 
-                avatar='https://source.unsplash.com/random' 
-                timeAgo='Yesterday at 5.00PM' 
-                content='I like the writing'
-            />            
+            <ApprovalCard>
+                <div>
+                    <h4>WARNING!!</h4>
+                    Are you sure you want to do this?
+                </div>
+
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail 
+                    author='Fernando' 
+                    avatar='https://source.unsplash.com/random' 
+                    timeAgo='Today at 3.00PM' 
+                    content='Nice blog post!'
+                /> 
+            </ApprovalCard>
+                       
+            <ApprovalCard>
+                <CommentDetail 
+                    author='Alex' 
+                    avatar='https://source.unsplash.com/random' 
+                    timeAgo='Today at 2.00AM' 
+                    content='I like the subject'
+                />
+            </ApprovalCard>            
+
+            <ApprovalCard>
+                <CommentDetail 
+                    author='Jane' 
+                    avatar='https://source.unsplash.com/random' 
+                    timeAgo='Yesterday at 5.00PM' 
+                    content='I like the writing'
+                />    
+            </ApprovalCard>
         </div>
     );
 };
